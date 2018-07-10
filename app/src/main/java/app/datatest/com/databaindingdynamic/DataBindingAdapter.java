af56c14b89aa4e850de6c7ab47bdb44e794d4640
Muhammed Thasneem P Android <muhammed.thasneem@yahoo.com>
2018-07-10 20:59:08 +0400

    final code

--- app/src/main/java/app/datatest/com/databaindingdynamic/DataBindingAdapter.java
+++ app/src/main/java/app/datatest/com/databaindingdynamic/DataBindingAdapter.java
@@ -1,22 +1,21 @@
 package app.datatest.com.databaindingdynamic;
 
 import android.databinding.BindingAdapter;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import java.util.List;
 
-import app.datatest.com.databaindingdynamic.databinding.EdittextLayoutBinding;
-
 public class DataBindingAdapter {
     @BindingAdapter("app:items")
     public static void setItems(ViewGroup viewGroup, List<MyModel> myModels) {
         for (MyModel myModel : myModels) {
-
             LayoutInflater inflate = LayoutInflater.from(viewGroup.getContext());
-            EdittextLayoutBinding layoutBinding = EdittextLayoutBinding.inflate(inflate);
-            layoutBinding.setItems(myModel);
-            viewGroup.addView(layoutBinding.getRoot());
+            ViewDataBinding binding = DataBindingUtil.inflate(inflate, R.layout.edittext_layout, viewGroup, false);
+            binding.setVariable(BR.items, myModel);
+            viewGroup.addView(binding.getRoot());
         }
 
     }
